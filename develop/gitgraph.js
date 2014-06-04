e606c0a3873e9a0482a4112a477b50c8c7d4b152
Lonewolf <nikhilkalige@gmail.com>
2014-06-05 02:40:35 +0530

    temporary commit

--- develop/gitgraph.js
+++ develop/gitgraph.js
@@ -18,10 +18,30 @@ GitGraph.prototype.set_config = function() {
 
 GitGraph.prototype.set_position = function(data) {
     var _i, _len;
+    var curr_row, curr_col;
+    curr_row = curr_col = 0;
+    this.branches = [];
+
     for(_i=0; _len = data.length, _i < _len; _i++) {
         var commit = data[_i];
+        var position = {};
 
+        // I am the first commit
+        if(_i == 0) {
+            position.row = position.column = 0;
+            commit.position = position;
+            continue;
+        }
+
+        position.row = curr_row++;
+        position.column = curr_col;
+        commit.position = position;
     }
 }
+
+GitGraph.prototype.render = function() {
+
+};
+
 
 module.exports = GitGraph;
